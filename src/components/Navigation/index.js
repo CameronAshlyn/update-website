1c5d5dfb4653fcb42d95277ed222ee28f62e11c4
Cameron Ashlyn <33036729+CameronAshlyn@users.noreply.github.com>
2019-05-20 20:00:10 -0400

    formats navigation to be a top nav

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,56 +1,65 @@
-//import React, { Fragment } from 'react'
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { Fragment } from "react";
+//import React from 'react'
+import { NavLink } from "react-router-dom";
 import logo from "../../assets/logo/blacklogo.svg";
 
 const Navigation = ({ navColor, themeColor }) => {
   const textStyle = { color: navColor };
   return (
-    <div>
-      <header
-        className="fixed left-0 top-0 w-100 flex items-center pv2 ph3 z-4 mobile"
-        style={{ backgroundColor: themeColor }}
-      >
-        <img className="db" width={64} height={40} src={logo} alt="fail" />
-      </header>
-
-      <Link className="nav-link nav-link--home not-mobile" to="/" exact>
-        <img src={logo} alt="fail" />
-      </Link>
-
-      <nav>
-        <NavLink
-          className="nav-link nav-link--about not-mobile"
-          activeClassName="nav-link--active"
-          to="/about"
-        >
-          <span data-label="ABOUT">
-            <span style={textStyle}>ABOUT</span>
-          </span>
+    <Fragment>
+      <nav className="nav-area dt w-100 absolute pa3  z-4 ">
+        <NavLink className="nav-link dtc v-mid w-25" to="/" exact>
+          <img className="dib w2 h2 br-100" width={64} height={40} src={logo} alt="fail" />
         </NavLink>
+        <div className=" nav-nav dtc v-mid w-75 tr">
+          <NavLink
+            className="nav-link link dim dark-gray f6 f5-ns dib mr3 mr4-ns"
+            activeClassName="nav-link--active"
+            to="/about"
+          >
+            <span data-label="ABOUT">
+              <span style={textStyle}>ABOUT</span>
+            </span>
+          </NavLink>
 
-        <NavLink
-          className="nav-link nav-link--contact not-mobile"
-          activeClassName="nav-link--active"
-          to="/contact"
-        >
-          <span data-label="CONTACT">
-            <span style={textStyle}>CONTACT</span>
-          </span>
-        </NavLink>
-        <a
-          className="nav-link nav-link--resume not-mobile"
-          activeClassName="nav-link--active"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://api-cameronashlyn.com/wp-content/uploads/2019/04/taylor-resume-april2019.pdf"
-        >
-          <span data-label="RESUME">
-            <span style={textStyle}>RESUME</span>
-          </span>
-        </a>
+
+          <a
+            className="nav-link link dim dark-gray f6 f5-ns dib mr3 mr4-ns"
+            activeClassName="nav-link--active"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://api-cameronashlyn.com/wp-content/uploads/2019/04/taylor-resume-april2019.pdf"
+          >
+            <span data-label="RESUME">
+              <span style={textStyle}>RESUME</span>
+            </span>
+          </a>
+
+
+        </div>
       </nav>
-    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </Fragment>
   );
 };
 
